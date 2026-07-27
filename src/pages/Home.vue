5d3806e721a5fb453bfbc264fd72b5d76db6b333
era760 <a21-6229@outlook.jp>
2026-07-27 09:12:04 +0900

    chore: adjust gap and add 'and more' button in Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -12,13 +12,14 @@ const openGitHub = () => {
     <p class="font-medium">Minecraftアドオン開発を、もっと簡単に。</p>
     <p class="font-medium text-sm text-zinc-400">動作確認済み環境：Windows 11 151.0.4129.15</p>
 
-    <div class="flex flex-col gap-7 mt-2">
+    <div class="flex flex-col gap-8 mt-2">
       <RouterLink to="/uuid" class="w-80">
         <BaseButton class="w-full">UUID Generator(Beta)</BaseButton>
       </RouterLink>
 
       <BaseButton class="w-80">Item JSON Generator(WIP)</BaseButton>
       <BaseButton class="w-80">Manifest Generator(WIP)</BaseButton>
+      <BaseButton class="w-80">and more!</BaseButton>
 
       <BaseButton class="w-80 bg-[#423f3f]! hover:!bg[#4d4a4a]" @click="openGitHub"> GitHub </BaseButton>
     </div>
